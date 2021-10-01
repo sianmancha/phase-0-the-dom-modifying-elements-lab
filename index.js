843a05644f212b174ef46d55fa388750c51240c2
Sian Mancha <slmancha20@gmail.com>
2021-10-01 18:06:26 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1")
+
+newHeader.idName = "victory"
+newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
+document.body.append(newHeader);
